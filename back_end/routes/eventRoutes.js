399286372871176216ab21b2b1b4ab01c2fed0ea
AnhQuanNe <quannguyen.dn2005@gmail.com>
2025-11-05 12:40:18 +0700

    update giao diện và đẩy event từ database lên banner

--- back_end/routes/eventRoutes.js
+++ back_end/routes/eventRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getEvents, getEventById } from "../controllers/eventController.js";
+import { getEvents, getEventById, getFeaturedEvents } from "../controllers/eventController.js";
 
 const router = express.Router();
 
+router.get("/featured", getFeaturedEvents); // 🆕 Thêm dòng này
 router.get("/search", getEvents);
 router.get("/:id", getEventById);
 
